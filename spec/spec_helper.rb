3d954ca4f5abc7034919432a5f5a56059f08b7f1
Alex Butirskiy <butirskiy@gmail.com>
2015-11-12 23:06:39 +0200

    Daily commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ CodeClimate::TestReporter.start
 require 'rspec'
 require 'bundler/setup'
 require 'rg_codebreaker'
+require 'codebreaker/console'
 
 RSpec.configure do |config|
 # config.filter_run_excluding statistic_test: true
