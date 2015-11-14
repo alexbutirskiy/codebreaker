812b5a98c501dc47083053df1d8ba574b69fa066
Alex Butirskiy <butirskiy@gmail.com>
2015-11-14 23:25:11 +0200

    Daily commit

--- lib/codebreaker/console.rb
+++ lib/codebreaker/console.rb
@@ -1,15 +1,59 @@
 require_relative 'game'
 require 'byebug'
 
-module Console
+module Command
+  HELP = 'help'
+  QUIT = 'quit'
+  HINT = 'hint'
+  SAVE = 'save'
+  RESTORE = 'restore'
+
+end
+
+module State
   GREETING = 1
   RESTORE = 2
   ASK_DIFFICULTY = 3
   GAME = 4
+end
 
-  @state = GREETING
-
+class Console_game
   include Codebreaker
+  include State
+
+  def initialize
+    @state = GREETING
+  end
+
+  def play inp
+    inp.downcase!
+    return command(inp) if Command.constants.map{|a| Command.const_get a}.include?(inp)
+    'Enter your command'
+  end
+
+  def command cmd
+    case cmd
+    when Command::HELP
+      help
+    when Command::QUIT
+      nil
+    when Command::HINT
+      'This is a hint'
+    when Command::SAVE
+      'This is a save'
+    when Command::RESTORE
+      'This is a restore'
+    else
+      raise ArgumentError, "Unknown command cmd"
+    end
+  end
+
+  def help
+<<-EOL
+        Help
+
+  end
+
     def console_game inp=nil
       return false if inp.upcase == 'QUIT'
 byebug
