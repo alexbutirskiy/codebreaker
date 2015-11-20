eb9218dd6b6bb757f8239fedad43418755b80a06
Alex Butirskiy <butirskiy@gmail.com>
2015-11-20 09:27:03 +0200

    'except' and 'only' options added to Saver

--- lib/codebreaker/saver.rb
+++ lib/codebreaker/saver.rb
@@ -3,12 +3,18 @@ require 'yaml'
 # Provides some methods to save and restore data
 module Codebreaker
   module Saver
-    def save arg = nil
+    def save(arg = nil, **options)
       filename = get_filename arg
       f = File.open(filename, 'w')
 
+      only = Array(options[:only]).map { |r| "@#{r}".to_sym }
+      except = Array(options[:except]).map { |r| "@#{r}".to_sym }
+      variables = instance_variables
+      variables -= except unless except.empty?
+      variables &= only unless only.empty?
+
       inst_state = {}
-      instance_variables.each do |var_name|
+      variables.each do |var_name|
         inst_state[var_name] = instance_variable_get(var_name)
       end
 
@@ -16,10 +22,17 @@ module Codebreaker
       f.close
     end
 
-    def restore arg = nil
+    def restore(arg = nil, **options)
+      only = Array(options[:only]).map { |r| "@#{r}".to_sym }
+      except = Array(options[:except]).map { |r| "@#{r}".to_sym }
+
       filename = get_filename arg
       f = File.open(filename, 'r')
       inst_state = YAML.load(f.read)
+
+      inst_state.keep_if { |key, _| only.include?(key) } unless only.empty?
+      inst_state.keep_if { |key, _| !except.include?(key) } unless except.empty?
+
       inst_state.each { |k, v| instance_variable_set(k, v) }
     end
 
@@ -30,6 +43,7 @@ module Codebreaker
       filename += '.yml' unless filename.match(/\.yml$/)
       filename
     end
+
   end
 end
 
